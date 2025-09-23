57b51915b583fd06c1f084c66da626b1bd33cf2c
samadariyan <samadariyan74@gmail.com>
2025-09-23 11:27:22 +0330

    lecture 110

---
+++ build.rs
@@ -0,0 +1,21 @@
+//
+// we have to take the help of CC crate to compile C code
+//
+
+// 1. set the cross compiler
+
+// 2. Add all C source files (.c files)
+
+// 3. Add all C include files (.h files)
+
+// 4. Add Define macros, -D (optional)
+
+// 5. Add os or .asm files (optional)
+
+// 6. Add compiler flags
+
+// 7. generate the static library
+
+// 8. you may communicate with the cargo from build script using println!() statements.
+
+fn main() {}
