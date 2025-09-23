078f662e02e407bc50a64537f47d10e510b6d068
samadariyan <samadariyan74@gmail.com>
2025-09-23 12:07:25 +0330

    lecture 111

--- build.rs
+++ build.rs
@@ -2,20 +2,53 @@
 // we have to take the help of CC crate to compile C code
 //
 
-// 1. set the cross compiler
+use std::fs;
 
-// 2. Add all C source files (.c files)
+fn main() {
+    let mut builder = cc::Build::new();
 
-// 3. Add all C include files (.h files)
+    // 1. set the cross compiler
+    builder.compiler("arm-none-eabi-gcc");
 
-// 4. Add Define macros, -D (optional)
+    // 2. Add all C source files (.c files)
+    // 2.a Add all C files from the HAL diver folder
+    // builder.file("c_src/lcd_tsc_mpu_drivers/Drivers/STM32F3xx_HAL_Driver/Src/file.c");
+    let hal_src_path = "c_src/lcd_tsc_mpu_drivers/Drivers/STM32F3xx_HAL_Driver/Src";
 
-// 5. Add os or .asm files (optional)
+    for entry in fs::read_dir(hal_src_path).expect("can not read hal driver from src folder") {
+        let path = entry.unwrap().path();
 
-// 6. Add compiler flags
+        if path.extension().and_then(|s| s.to_str()) == Some("c") {
+            println!("Compiling {:?}", path);
+            builder.file(&path);
+        }
+    }
 
-// 7. generate the static library
+    // 2.b Add all .c files from core/src
+    let core_src_path = "c_src/lcd_tsc_mpu_drivers/Core/Src";
 
-// 8. you may communicate with the cargo from build script using println!() statements.
+    for entry in fs::read_dir(core_src_path).expect("can not read core file from src folder") {
+        let path = entry.unwrap().path();
 
-fn main() {}
+        if path.extension().and_then(|s| s.to_str()) == Some("c") {
+            println!("Compiling {:?}", path);
+            builder.file(&path);
+        }
+    }
+
+    // 3. Add all C include files (.h files)
+    builder.include("c_src/lcd_tsc_mpu_drivers/Core/Inc");
+    builder.include("c_src/lcd_tsc_mpu_drivers/Drivers/STM32F3xx_HAL_Driver/Inc");
+    builder.include("c_src/lcd_tsc_mpu_drivers/Drivers/CMSIS/Include");
+    builder.include("c_src/lcd_tsc_mpu_drivers/Drivers/CMSIS/Device/ST/STM32F3xx/Include");
+
+    // 4. Add Define macros, -D (optional)
+
+    // 5. Add os or .asm files (optional)
+
+    // 6. Add compiler flags
+
+    // 7. generate the static library
+
+    // 8. you may communicate with the cargo from build script using println!() statements.
+}
